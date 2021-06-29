8c78190ba1587f282ecf8fdf0cf069e1f56ccea2
Victor <desarrollo5@aveonline.co>
2021-06-29 14:49:19 -0500

    se queman las variables de conexion a la base de datos

--- src/db/index.js
+++ src/db/index.js
@@ -1,34 +1,34 @@
-import {createPool} from 'mysql'
-import  dotenv from 'dotenv';
+import { createPool } from 'mysql';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const options = {
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASS,
-  database: process.env.DATABASE,
-}
+	host: '192.168.0.15',
+	user: 'w1avelog',
+	password: 'tlhOqe2B4fGsDeQ2',
+	database: 'c1wcwlogistics'
+};
 
-const pool = createPool(options)
+const pool = createPool(options);
 
 export function execQuery(query = '', params = []) {
-  return new Promise((resolve, reject) => {
-    pool.getConnection((errorPool, connection) => {
-      if (errorPool) {
-        console.log(`ErrorPool: ${errorPool}`)
-        return createPool(options)
-      } else {
-        connection.query(query, params, (error, results, _) => {
-          connection.release()
-          if (error) {
-            console.log(`ErrorQuery: ${error}`)
-            reject(error)
-          }
-          return resolve(results)
-        })
-      }
-    })
-  })
+	return new Promise((resolve, reject) => {
+		pool.getConnection((errorPool, connection) => {
+			if (errorPool) {
+				console.log(`ErrorPool: ${errorPool}`);
+				return createPool(options);
+			} else {
+				connection.query(query, params, (error, results, _) => {
+					connection.release();
+					if (error) {
+						console.log(`ErrorQuery: ${error}`);
+						reject(error);
+					}
+					return resolve(results);
+				});
+			}
+		});
+	});
 }
 
 // export function execMutation({mutation = '', params = [], method = ''}) {
