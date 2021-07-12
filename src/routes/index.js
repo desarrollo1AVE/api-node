c7f2a2454bf256b23eb357ff45bde29823390675
Victor <desarrollo5@aveonline.co>
2021-07-12 07:55:01 -0500

    se agrega la ruta de costos

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import novelty from './novelty/index.js';
+import cost from './cost/index.js';
 
 const router = express()
 
 router.use('/novelty', novelty)
+router.use('/cost', cost)
 
 
 
